7b363cb9cf286bec85f93aeb47285e3c3f5c59c5
Josema <josema@MacBook-Pro-de-Josema.local>
2019-03-06 16:49:13 +0100

    Ejemplos de bucles for, while y do while

---
+++ Script-P10a.js
@@ -0,0 +1,30 @@
+	//Primero el ejemplo del bucle for
+	//Ejecuta las instrucciones que se encuentran dentro mientras la expresión sea verdadera
+	var a = 3;
+	var b = 2;
+	console.log("muestra el valor de a y b", a, b)
+	for(a=0;a<5;a++){
+  		b = b + 2;
+  	console.log("muestra el valor de b", b);
+	}
+
+
+	var cont = 0;
+	while(cont < 5){
+  	cont = cont + 1;
+	console.log(cont);
+	}
+
+	var num = 0;
+	do{
+  	num = num + 1;
+	}while(num < 5);
+	console.log(num);
+	
+
+
+
+
+
+
+	
